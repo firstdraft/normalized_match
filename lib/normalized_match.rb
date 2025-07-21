b892e988b5babde167ea11c53958a0b3a5057cbf
Raghu Betina <raghu@firstdraft.com>
2025-07-21 00:36:31 -0500

    Update CI to test multiple Ruby versions
    - Test Ruby 3.0-3.4 instead of single version
    - Match CI configuration with other gems

--- lib/normalized_match.rb
+++ lib/normalized_match.rb
@@ -6,10 +6,3 @@ require_relative "normalized_match/normalized_match_matcher"
 # Main namespace.
 module NormalizedMatch
 end
-
-# Include the matcher in RSpec when this gem is loaded
-if defined?(RSpec)
-  RSpec.configure do |config|
-    config.include NormalizedMatch::NormalizedMatchMatcher
-  end
-end
